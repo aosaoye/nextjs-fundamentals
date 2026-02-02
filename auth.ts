61a9d90e915194b17329e0a88ba058e3843dbc87
aosaoye <a29322@svalero.com>
2026-02-02 22:57:22 +0100

    feat: Add `SideNav` component with sign-out functionality, refine `User` type and `getUser` return in `auth.ts`, and update Next.js type reference path.

--- auth.ts
+++ auth.ts
@@ -1,26 +1,27 @@
-import NextAuth, { User } from "next-auth";
+import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import postgres from "postgres";
+import { User } from '@/app/lib/definitions';
 
 
 const sql = postgres(process.env.DATABASE_URL!, {
     ssl: 'require'
 })
 
-async function getUser(email: string): Promise<User | null> {
-    
+async function getUser(email: string): Promise<User | undefined> {
+
     try {
         const user = await sql<User[]>`SELECT * FROM users WHERE email = ${email}`;
-        return user[0] || null;
+        return user[0];
     } catch (error) {
         console.error('Failed to fetch user:', error);
         throw new Error('Failed to fetch user.');
     }
 }
 
-export const {auth, signIn, signOut} = NextAuth({ 
+export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     providers: [Credentials({
         async authorize(credentials) {
