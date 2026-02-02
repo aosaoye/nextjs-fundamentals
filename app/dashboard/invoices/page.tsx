ec04f557bb27a69a35d26ee739dda593b44efea0
aosaoye <a29322@svalero.com>
2026-02-02 12:42:25 +0100

    feat: add invoices dashboard page with search and create invoice components

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,29 @@
-export default function InvoicePage() {
-    return <h1 className="text-xl text-gray-800 md:text-2xl md:leading-normal">Invoices Page</h1>;
-};
+
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { DM_Sans } from 'next/font/google';
+ 
+const dmSans = DM_Sans({ subsets: ['latin'], weight: ['400', '700'] });
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${dmSans.className} text-xl text-gray-800 md:text-2xl md:leading-normal`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
